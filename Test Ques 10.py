cd0edcf4cc42ae3f9bbcc6c52df006686be6849c
NeerajKumar1407 <60864988+NeerajKumar1407@users.noreply.github.com>
2020-03-31 19:12:14 +0530

    Add files via upload

---
+++ Test Ques 10.py
@@ -0,0 +1,43 @@
+"""Given two two interger arrays. find the longest common subsequence.
+e.g: a=[1 5 2 6 3 7], b=[5 6 7 1 2 3]. return[1 2 3] or [5 6 7]"""
+
+
+def lcs(X, Y, m, n): 
+    L = [[0 for x in range(n+1)] for x in range(m+1)] 
+
+    for i in range(m+1): 
+        for j in range(n+1): 
+            if i == 0 or j == 0: 
+                L[i][j] = 0
+            elif X[i-1] == Y[j-1]: 
+                L[i][j] = L[i-1][j-1] + 1
+            else: 
+                L[i][j] = max(L[i-1][j], L[i][j-1]) 
+  
+    index = L[m][n] 
+  
+    lcs = [""] * (index+1) 
+    lcs[index] = "" 
+
+    i = m 
+    j = n 
+    while i > 0 and j > 0: 
+  
+        if X[i-1] == Y[j-1]: 
+            lcs[index-1] = X[i-1] 
+            i-=1
+            j-=1
+            index-=1
+ 
+        elif L[i-1][j] > L[i][j-1]: 
+            i-=1
+        else: 
+            j-=1
+  
+    print ("LCS of " + X + " and " + Y + " is " + "".join(lcs))  
+ 
+X = "152637"
+Y = "567123"
+m = len(X) 
+n = len(Y) 
+lcs(X, Y, m, n)
